6b34d456f5e449f334453f91cdb688f9110ab2be
Stefan Mitic <stefan.mitic@elfak.rs>
2018-07-25 23:28:42 +0200

    bootstrap

--- RestaurantApp/Startup.cs
+++ RestaurantApp/Startup.cs
@@ -64,16 +64,13 @@ namespace RestaurantApp
 
             app.UseStaticFiles();
 
+            app.UseNodeModules(env.ContentRootPath);
+
             app.UseAuthentication();
 
             app.UseMvc(ConfigureRoutes);
 
-            app.Run(async (context) =>
-            {
-                var greeting = greeter.GetMessageOfTheDay();
-                context.Response.ContentType = "text/plain";//mime type
-                await context.Response.WriteAsync($"Not found");
-            });
+
         }
 
         private void ConfigureRoutes(IRouteBuilder routeBuilder)
